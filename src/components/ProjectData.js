8f06f0ea1238c3488a2f2d0131b359a5520b6819
Stephen Blevins <61333980+sblevins-dev@users.noreply.github.com>
2026-04-27 12:13:28 -0400

    changes

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -15,7 +15,6 @@ const ProjectData =
             "technologies": [
                 ".NET Core, Angular, AWS Elastic Beanstalk, AWS RDS"
             ],
-            "demoLink": "https://mk12-ftf8ddcwajb6b6cn.canadacentral-01.azurewebsites.net/",
             "sourceLink": "https://github.com/sblevins-dev/MK12"
         },
         {
